8517ecaac3dab725b9206a4ca0215c3eb39485e0
Logan Connolly <logan.connolly@protonmail.com>
2020-01-26 21:53:48 +0100

    Simplify tests

--- mychef-scraper/tests/resources/test_sources.py
+++ mychef-scraper/tests/resources/test_sources.py
@@ -1,32 +1,24 @@
 import pytest
 import falcon
-import msgpack
 
 from falcon import testing
 
 from mychef_scraper.app import api
 
 
-@pytest.mark.usefixtures("doc")
 class TestSources:
     @pytest.fixture(scope="class")
     def client(self):
         yield testing.TestClient(api)
 
     def test_list_sources(self, client):
-        response = client.simulate_get('/sources')
-        assert response.status == falcon.HTTP_OK
+        doc = client.simulate_get('/sources')
+        assert doc.status == falcon.HTTP_OK
 
-    def test_get_source(self, client, doc):
-        sid = 1
-        response = client.simulate_get(f'/source/{sid}')
-        result_doc = msgpack.unpackb(response.content, raw=False)
-        assert result_doc == {"source": doc["sources"][0]}
-        assert response.status == falcon.HTTP_OK
+    def test_get_source(self, client):
+        resp = client.simulate_get(f'/source/{1}')
+        assert resp.status == falcon.HTTP_OK
 
-    def test_get_source_not_found(self, client, doc):
-        sid = -1
-        response = client.simulate_get(f'/source/{sid}')
-        result_doc = msgpack.unpackb(response.content, raw=False)
-        assert result_doc == {"message": "Source not found."}
-        assert response.status == falcon.HTTP_OK
+    def test_get_source_not_found(self, client):
+        resp = client.simulate_get(f'/source/{-1}')
+        assert resp.status == falcon.HTTP_404
