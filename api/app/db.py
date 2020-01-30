f97cf722f2503e406395bb9e13a14b9b9cd52775
Logan Connolly <logan.connolly@protonmail.com>
2020-01-30 17:04:02 +0100

    Add postgresql database to application

---
+++ api/app/db.py
@@ -0,0 +1,14 @@
+import os
+
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+database = Database(DATABASE_URL)
+
