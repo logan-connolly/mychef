cde1ce2e868179b4ae4557986b206f14d6912990
Logan Connolly <logan.connolly@protonmail.com>
2020-01-30 17:45:49 +0100

    Add source model to DB

--- api/app/db.py
+++ api/app/db.py
@@ -3,12 +3,16 @@ import os
 from databases import Database
 from sqlalchemy import create_engine, MetaData
 
+from .models.sources import Source
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
+
+# Init Models
+sources = Source(metadata).create_table()
 
 database = Database(DATABASE_URL)
 
