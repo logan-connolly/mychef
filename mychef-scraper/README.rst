bc91c053a876c3ce39f41cbdc7b21ce5b8de9c50
Logan Connolly <logan.connolly@protonmail.com>
2020-01-30 14:57:51 +0100

    Reorganize project from top down

--- mychef-scraper/README.rst
+++
@@ -1,7 +0,0 @@
-mychef-scraper
-==============
-
-what is it?
------------
-
-    mychef-scraper is a package dedicated to scraping recipes from selected sites.
--- mychef-scraper/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-GUNICORN=/venv/bin/gunicorn
-CONF=$PWD/mychef_scraper/config/gunicorn_conf.py
-$GUNICORN app:api --chdir mychef_scraper -c $CONF --reload
