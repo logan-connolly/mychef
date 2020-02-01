0a7f77ea4fa3e88d3f8d97a9ed1990e902089474
Logan Connolly <logan.connolly@protonmail.com>
2020-02-01 21:30:33 +0100

    Add recipes route to application

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api import sources
+from app.api import sources, recipes
 from app.db import engine, metadata, database
 
 
@@ -20,3 +20,4 @@ async def shutdown():
 
 
 app.include_router(sources.router, prefix="/sources", tags=["sources"])
+app.include_router(recipes.router, prefix="/sources/{sid}/recipes", tags=["recipes"])
