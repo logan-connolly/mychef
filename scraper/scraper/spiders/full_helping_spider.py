effc123b6dbc72fab69763813234d1e3653fd229
Logan Connolly <logan.connolly@protonmail.com>
2020-03-24 19:18:49 +0100

    Refactor spider to start scraping at the most recent recipe. Will need to include ingredient extraction later

--- scraper/scraper/spiders/full_helping_spider.py
+++ scraper/scraper/spiders/full_helping_spider.py
@@ -4,71 +4,38 @@ import string
 import requests
 
 
-class FullHelpingSpider(scrapy.Spider):
-    """Scrapes the website <thefullhelping.com> for recipes"""
+def get_latest_recipe():
+    index_url = "https://www.thefullhelping.com/recipe-index/",
+    return ["https://www.thefullhelping.com/spicy-cabbage-chickpea-soup/"]
 
+
+class FullHelpingSpider(scrapy.Spider):
+    """Scrapes the website <thefullhelping.com> and posts to mychef"""
     name = "full_helping"
     sid = 1
     download_delay = 8
-
-    start_urls = [
-        "https://www.thefullhelping.com/recipe-index/",
-    ]
+    start_urls = get_latest_recipe()
 
     def parse(self, response):
-        for post in response.css(".single-posty"):
+        if not response.css(".wprm-recipe-ingredients-contain"):
+            continue
 
-            category = post.css('.cat div a::text').get()
-            if not self.is_recipe(category):
-                continue
+        payload = {
+            "name": response.css(".title::text").get(),
+            "url": response.url,
+            "image": self.get_image_url(response),
 
-            # Extract meta information from recipe index page
-            payload = {
-                "url": post.css("a::attr(href)").get(),
-                "name": post.css(".title a::text").get(),
-                "image": self.get_image_url(post),
-            }
-            requests.post(f"http://localhost:8002/sources/{self.sid}/recipes/", json=payload)
+        }
+        requests.post(f"http://localhost:8002/sources/{self.sid}/recipes/", json=payload)
 
         next_page_link = response.css(".nav-previous a::attr(href)").get()
         if next_page_link:
             next_page = response.urljoin(next_page_link)
             yield scrapy.Request(url=next_page, callback=self.parse)
 
-    def get_image_url(self, post):
-        attrs = ['style', 'data-bg']
-        for attr in attrs:
-            selection = post.css(f"#hero-image::attr({attr})").re(r'(http.*)\"')
-            if selection:
-                return selection[0]
+    def get_image_url(self, response):
+        img = response.css("p > img").get()
+        return img.re_first(r'src="(http.*?)\"')
 
-    def is_recipe(self, category):
-        cats = [
-            "food and healing",
-            "nutrition and wellness"
-        ]
-        return True if category.lower() not in cats else False
-
-
-class FullHelpingRecipe(scrapy.Spider):
-    """Scrape recipe for individual recipes"""
-
-    name = "full_helping_recipe"
-    sid = 1
-    download_delay = 8
-    nlp = spacy.load('en')
-
-    def start_requests(self):
-        urls = [
-            "https://www.thefullhelping.com/creamy-cauliflower-turmeric-kale-soup/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        ingredients_css = response.css(".wprm-recipe-ingredient-name::text")
-        ingredients = " ".join(i.get() for i in ingredients_css)
-        remove_punct = "".join(c for c in ingredients if c not in string.punctuation)
-        doc = self.nlp(remove_punct)
-        yield {"ingredients": [token for token in doc if token]}
-
+    def get_ingredients(self, response):
+        pass
