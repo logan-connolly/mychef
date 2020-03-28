4ce71a0dd6233ab20f8981fe9ab4819aa27ea2ee
Logan Connolly <logan.connolly@protonmail.com>
2020-03-28 16:41:13 +0100

    Rename functions in tests as well

--- scraper/tests/spiders/conftest.py
+++ scraper/tests/spiders/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.fixture
-def full_helping_homepage():
+def full_helping_recipe_index():
     fname = "webpages/full_helping.html"
     fpath = Path(__file__).parent.joinpath(fname)
     with open(fpath) as f:
--- scraper/tests/spiders/test_full_helping.py
+++ scraper/tests/spiders/test_full_helping.py
@@ -3,14 +3,14 @@ import pytest
 from scraper.spiders.full_helping_spider import UrlExtractor
 
 
-@pytest.mark.usefixtures("full_helping_homepage")
-def test_get_latest_recipe(full_helping_homepage, monkeypatch):
+@pytest.mark.usefixtures("full_helping_recipe_index")
+def test_get_latest_recipe(full_helping_recipe_index, monkeypatch):
 
-    def mock_get_homepage(url):
-        return full_helping_homepage
+    def mock_get_start_page(url):
+        return full_helping_recipe_index
 
-    monkeypatch.setattr(UrlExtractor, "get_homepage", mock_get_homepage)
+    monkeypatch.setattr(UrlExtractor, "get_start_page", mock_get_start_page)
 
-    start_url = UrlExtractor("http://example.com").get_recipe_url()
+    start_url = UrlExtractor("http://thefullhelping.com").get_recipe_url()
     expected_url = ["https://www.thefullhelping.com/chai-spice-energy-balls/"]
     assert start_url == expected_url
