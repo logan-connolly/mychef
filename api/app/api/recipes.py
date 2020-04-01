fa93f1b8a00a8bfedb3bde4c4afe5d4dd18a521c
Logan Connolly <logan.connolly@protonmail.com>
2020-04-02 00:21:38 +0200

    Refactor recipes api like sources

--- api/app/api/recipes.py
+++ api/app/api/recipes.py
@@ -2,6 +2,7 @@ from typing import List
 
 from asyncpg.exceptions import ForeignKeyViolationError
 from fastapi import APIRouter, HTTPException
+from orm.exceptions import NoMatch
 
 from app.models.recipes import Recipe, RecipeDB, RecipeSchema
 
@@ -11,16 +12,15 @@ router = APIRouter()
 
 @router.post("/", response_model=RecipeDB, status_code=201)
 async def add_recipe(sid: int, payload: RecipeSchema):
-    if not await Recipe.objects.get(url=payload.url):
-        raise HTTPException(status_code=400, detail="URL already exists")
-
     try:
-        recipe_id = await CRUD.post(sid, payload)
+        recipe = await Recipe.objects.create(
+            name=payload.name, url=payload.url, image=payload.image, sid=sid
+        )
     except ForeignKeyViolationError:
         raise HTTPException(status_code=404, detail="Source does not exist")
 
     response_object = {
-        "id": recipe_id,
+        "id": recipe.pk,
         "name": payload.name,
         "url": payload.url,
         "image": payload.image,
@@ -31,69 +31,27 @@ async def add_recipe(sid: int, payload: RecipeSchema):
 
 @router.get("/{id}/", response_model=RecipeDB, status_code=200)
 async def get_recipe(sid: int, id: int):
-    recipe = await CRUD.get(sid, id)
-    if not recipe:
-        raise HTTPException(status_code=404, detail="Recipe not found")
+    try:
+        recipe = await Recipe.objects.get(sid=sid, id=id)
+    except NoMatch:
+        raise HTTPException(status_code=404, detail="Source not found")
     return recipe
 
 
 @router.get("/", response_model=List[RecipeDB], status_code=200)
 async def list_recipes(sid: int):
-    recipes = await CRUD.get_all(sid)
-    if not recipes:
-        raise HTTPException(status_code=404, detail="No recipes found for source")
-    return recipes
+    return await Recipe.objects.get(sid=sid).all()
 
 
 @router.put("/{id}/", response_model=RecipeDB, status_code=200)
 async def update_recipe(sid: int, id: int, payload: RecipeSchema):
-    recipe = await CRUD.get(sid, id)
-    if not recipe:
-        raise HTTPException(status_code=404, detail="Recipe not found")
-
-    recipe_id = await CRUD.put(sid, id, payload)
-
-    response_object = {
-        "id": recipe_id,
-        "name": payload.name,
-        "url": payload.url,
-        "image": payload.image,
-        "sid": sid,
-    }
-    return response_object
+    recipe = await get_recipe(sid, id)
+    await recipe.update(name=payload.name, url=payload.url, image=payload.image)
+    return recipe
 
 
 @router.delete("/{id}/", response_model=RecipeDB, status_code=200)
 async def remove_recipe(sid: int, id: int):
-    recipe = await CRUD.get(sid, id)
-    if not recipe:
-        raise HTTPException(status_code=404, detail="Recipe not found")
-
-    await CRUD.delete(sid, id)
+    recipe = await get_recipe(sid, id)
+    await recipe.delete()
     return recipe
-
-
-class CRUD:
-    @staticmethod
-    async def post(sid: int, payload: RecipeSchema):
-        return await Recipe.objects.create(name=payload.name, url=payload.url, sid=sid)
-
-    @staticmethod
-    async def get(sid: int, id: int):
-        return await Recipe.objects.get(sid=sid, id=id)
-
-    @staticmethod
-    async def get_all(sid: int):
-        return await Recipe.objects.all()
-
-    @staticmethod
-    async def put(sid: int, id: int, payload: RecipeSchema):
-        recipe = await Recipe.objects.get(sid=sid, id=id)
-        return await recipe.update(
-            name=payload.name, url=payload.url, image=payload.image
-        )
-
-    @staticmethod
-    async def delete(sid: int, id: int):
-        recipe = await Recipe.objects.get(sid=sid, id=id)
-        return await recipe.delete()
