2afd5f1fd91a0471678cd73128dc4938a6c8124f
Logan Connolly <logan.connolly@protonmail.com>
2020-04-01 21:29:55 +0200

    Update db file to use orm and session maker

--- api/app/db.py
+++ api/app/db.py
@@ -2,12 +2,19 @@ import os
 
 from databases import Database
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from .models.base import Base
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# connect engine to database and create all tables
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(engine)
+
+# create and configure "Session" to be used throughout app
+Session = sessionmaker(bind=engine)
+session = Session()
+
 database = Database(DATABASE_URL)
