30f1d4f62b4b50e6e1500ae46d9f3ff4afa2acbd
Logan Connolly <logan.connolly@protonmail.com>
2020-04-04 12:37:45 +0200

    Add test skeleton - need to find out how to properly test. Having issues with monkey patching the orm functionality.

---
+++ api/tests/api/test_sources.py
@@ -0,0 +1,15 @@
+class TestSources:
+    def test_add_source(self, test_app):
+        pass
+
+    def test_get_source(self, test_app, monkeypatch):
+        pass
+
+    def test_get_sources(self):
+        pass
+
+    def test_update_source(self):
+        pass
+
+    def test_remove_source(self):
+        pass
