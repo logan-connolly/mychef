d3fd07a5e04c9f994ea7fdbe6db35089188ce904
Logan Connolly <logan.connolly@protonmail.com>
2020-04-05 17:22:24 +0200

    Add descriptive status codes from starlette

--- api/src/api/sources.py
+++ api/src/api/sources.py
@@ -3,6 +3,12 @@ from typing import List
 from asyncpg.exceptions import UniqueViolationError
 from fastapi import APIRouter, HTTPException
 from orm.exceptions import NoMatch
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+)
 
 from ..models.sources import Source, SourceDB, SourceSchema
 
@@ -10,12 +16,14 @@ from ..models.sources import Source, SourceDB, SourceSchema
 router = APIRouter()
 
 
-@router.post("/", response_model=SourceDB, status_code=201)
+@router.post("/", response_model=SourceDB, status_code=HTTP_201_CREATED)
 async def add_source(payload: SourceSchema):
     try:
         source = await Source.objects.create(name=payload.name, url=payload.url.host)
     except UniqueViolationError:
-        raise HTTPException(status_code=400, detail="Source url already exists")
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST, detail="Source url already exists"
+        )
 
     response_object = {
         "id": source.pk,
@@ -25,38 +33,40 @@ async def add_source(payload: SourceSchema):
     return response_object
 
 
-@router.get("/{id}/", response_model=SourceDB, status_code=200)
+@router.get("/{id}/", response_model=SourceDB, status_code=HTTP_200_OK)
 async def get_source(id: int):
     try:
         source = await Source.objects.get(id=id)
     except NoMatch:
-        raise HTTPException(status_code=404, detail="Source not found")
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Source not found")
     return source
 
 
-@router.get("/", response_model=List[SourceDB], status_code=200)
+@router.get("/", response_model=List[SourceDB], status_code=HTTP_200_OK)
 async def get_sources(domain: str = None):
     if domain:
         sources = await Source.objects.filter(url__contains=domain).all()
         if not sources:
             message = f"No sources matching '{domain}' domain pattern"
-            raise HTTPException(status_code=404, detail=message)
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=message)
         return sources
 
     sources = await Source.objects.all()
     if not sources:
-        raise HTTPException(status_code=404, detail="Could not find any sources")
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail="Could not find any sources"
+        )
     return sources
 
 
-@router.put("/{id}/", response_model=SourceDB, status_code=200)
+@router.put("/{id}/", response_model=SourceDB, status_code=HTTP_200_OK)
 async def update_source(id: int, payload: SourceSchema):
     source = await get_source(id)
     await source.update(name=payload.name, url=payload.url)
     return source
 
 
-@router.delete("/{id}/", response_model=SourceDB, status_code=200)
+@router.delete("/{id}/", response_model=SourceDB, status_code=HTTP_200_OK)
 async def remove_source(id: int):
     source = await get_source(id)
     await source.delete()
