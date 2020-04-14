dfbd0e90e0b93382f999859b71607bfe26b6e52c
Logan Connolly <logan.connolly@protonmail.com>
2020-04-14 23:48:36 +0200

    Add fixtures for testing async application and create db tables for each class test in the test_db

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,10 +1,43 @@
-import pytest
-from starlette.testclient import TestClient
+import asyncio
 
-from src.main import app
+from multiprocessing import Process
+
+import aiohttp
+import pytest
+import uvicorn
+
+from sqlalchemy import create_engine
+
+from src import config, main, db
+
+
+@pytest.yield_fixture(scope="module")
+def event_loop(request):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope="module")
-def test_app():
-    client = TestClient(app)
-    yield client  # testing happens here
+async def server():
+    kwargs = dict(host="127.0.0.1", port=5000, log_level="info")
+    proc = Process(target=uvicorn.run, args=(main.app,), kwargs=kwargs, daemon=True)
+    proc.start()
+    await asyncio.sleep(0.2)
+    yield
+    proc.kill()
+
+
+@pytest.fixture(scope="module")
+async def session():
+    async with aiohttp.ClientSession() as session:
+        yield session
+
+
+@pytest.fixture(scope="class")
+def create_tables():
+    test_url = config.settings.test_db_url
+    engine = create_engine(test_url)
+    db.metadata.create_all(engine)
+    yield
+    db.metadata.drop_all(engine)
