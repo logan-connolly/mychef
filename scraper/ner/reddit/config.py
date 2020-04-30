9c6f031322d6bc1fb07e08348e86160d5c1c0eb2
Logan Connolly <logan.connolly@protonmail.com>
2020-04-30 17:31:38 +0200

    Add module for downloading reddit recipe comment data.

---
+++ scraper/ner/reddit/config.py
@@ -0,0 +1,19 @@
+from praw import Reddit
+from pydantic import BaseSettings, SecretStr
+
+
+class RedditConfig(BaseSettings):
+    client_id: str
+    client_secret: str
+    password: str
+    username: str
+
+
+def connect_reddit(config: RedditConfig):
+    return Reddit(
+        user_agent=f"mychef by /u/{config.username}",
+        client_id=config.client_id,
+        client_secret=config.client_secret,
+        password=config.password,
+        username=config.username,
+    )
