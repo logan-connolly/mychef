737c48c9e76b09cab1d37eef61363ed7395fb6e1
Logan Connolly <logan.connolly@protonmail.com>
2020-06-27 00:08:17 +0200

    Update main and config files to use event handlers and model

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -12,7 +12,13 @@ class PostgresSettings(BaseSettings):
 
 
 class Settings(BaseSettings):
+    DEBUG: bool = True
+
+    API_TITLE: str = "MyChef"
     API_V1_STR: str = "/api/v1"
+    OPENAPI_URL: str = f"{API_V1_STR}/openapi.json"
+
+    MODEL_PATH: str = ""
 
     PG = PostgresSettings()
     URI: PostgresDsn = f"postgres://{PG.user}:{PG.password}@{PG.host}/{PG.db}"
@@ -22,3 +28,10 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+api_settings = {
+    "title": settings.API_TITLE,
+    "openapi_url": settings.OPENAPI_URL,
+    "debug": settings.DEBUG,
+}
--- api/app/main.py
+++ api/app/main.py
@@ -1,23 +1,25 @@
 from fastapi import FastAPI
 
 from .api.api_v1.api import api_router
-from .core.config import settings
-from .db.database import database, engine, metadata
+from .core.config import api_settings, settings
+from .core.event_handlers import start_app_handler, stop_app_handler
+from .db.database import engine, metadata
+
+
+metadata.create_all(engine)
 
 
 def get_app():
 
-    app = FastAPI(title="mychef", openapi_url=f"{settings.API_V1_STR}/openapi.json")
-
-    metadata.create_all(engine)
+    app = FastAPI(**api_settings)
 
     @app.on_event("startup")
     async def startup():
-        await database.connect()
+        await start_app_handler(app)
 
     @app.on_event("shutdown")
     async def shutdown():
-        await database.disconnect()
+        await stop_app_handler(app)
 
     app.include_router(api_router, prefix=settings.API_V1_STR)
 
