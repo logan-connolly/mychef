f6a8f9d4d9ace2219f26e6cdaa48fbe8ab90a9bb
Logan Connolly <logan.connolly@protonmail.com>
2020-07-09 11:21:40 +0200

    Add CORS Middleware to allow frontend to communicate with API

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from .api.api_v1.api import api_router
 from .core.config import api_settings, settings
@@ -20,6 +21,15 @@ def get_app():
     @app.on_event("shutdown")
     async def shutdown():
         await stop_app_handler(app)
+
+    if settings.BACKEND_CORS_ORIGINS:
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
     app.include_router(api_router, prefix=settings.API_V1_STR)
 
