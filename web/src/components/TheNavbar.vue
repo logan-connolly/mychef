3138a44629a582b8f2100d23d66444486c6f2e0e
Logan Connolly <logan.connolly@protonmail.com>
2020-07-10 11:54:57 +0200

    Move the about dialog into the Navbar component

--- web/src/components/TheNavbar.vue
+++ web/src/components/TheNavbar.vue
@@ -5,7 +5,40 @@
     dense
     color="white"
   >
-    <TheNavbarAbout/>
+    <v-spacer></v-spacer>
+
+    <v-btn
+      @click.stop="dialog = true"
+      color="primary"
+      text>
+      <span class="secondary--text mr-2">About</span>
+      <v-icon>mdi-information-outline</v-icon>
+    </v-btn>
+
+    <v-dialog
+      v-model="dialog"
+      max-width="500"
+    >
+      <v-card>
+        <v-card-title class="headline font-weight-bold">Why MyChef</v-card-title>
+
+        <v-card-text class="headline font-weight-light">
+          We put all your favorite plant-based recipes in one place. On top of that, we recommend what to make based on what you have at home. No Ads, no tracking, just helping you decide what to make.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
     <v-btn
       href="https://github.com/logan-connolly/mychef"
@@ -20,15 +53,10 @@
 </template>
 
 <script>
-import TheNavbarAbout from './TheNavbarAbout.vue';
-
 export default {
   name: 'TheNavbar',
-  components: {
-    TheNavbarAbout
-  },
   data: () => ({
-    //
+    dialog: false
   })
 }
 </script>
