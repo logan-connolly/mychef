9c5b89378c14458cfda868c75d099f9f53876a6a
Logan Connolly <logan.connolly@protonmail.com>
2020-07-17 08:32:02 +0200

    Add div in recipe list to separate alerts and recipe list.

--- web/src/components/RecipeList.vue
+++ web/src/components/RecipeList.vue
@@ -1,22 +1,22 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <v-alert type="error" v-if="erorred">
-        There was an issue retrieving recipes.
-      </v-alert>
-      <v-alert type="warning" v-if="!loading && recipes == null">
-        No recipes were found in the database.
-      </v-alert>
-    </v-col>
-    <v-col
-      v-for="(recipe, n) in recipes"
-      :key="n"
-      class="d-flex child-flex"
-      cols="12" xl="3" lg="4" md="4" sm="6" xs="12"
-    >
-        <RecipeListCard :recipe="recipe"/>
-    </v-col>
-  </v-row>
+  <div class="text-center">
+    <v-alert type="error" v-if="erorred">
+      There was an issue retrieving recipes.
+    </v-alert>
+    <v-alert type="warning" v-if="!loading && recipes == null">
+      No recipes were found in the database.
+    </v-alert>
+    <v-row>
+      <v-col
+        v-for="(recipe, n) in recipes"
+        :key="n"
+        class="d-flex child-flex"
+        cols="12" xl="3" lg="4" md="4" sm="6" xs="12"
+      >
+          <RecipeListCard :recipe="recipe"/>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
