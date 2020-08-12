9664e43513c0e1871b7ce1f47aa2d5830ba95244
Logan Connolly <logan.connolly@protonmail.com>
2020-08-12 07:30:37 +0200

    Dynamically add web port to avoid CORS

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -13,19 +13,26 @@ class PostgresSettings(BaseSettings):
         env_prefix = "POSTGRES_"
 
 
+class WebSettings(BaseSettings):
+    port: int
+
+    class Config:
+        env_prefix = "WEB_"
+
+
 class Settings(BaseSettings):
     DEBUG: bool = True
+    MODEL: str = ""
 
     API_TITLE: str = "MyChef"
     API_V1_STR: str = "/api/v1"
     OPENAPI_URL: str = f"{API_V1_STR}/openapi.json"
 
+    WEB = WebSettings()
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
         "http://localhost",
-        "http://localhost:8080",
+        f"http://localhost:{WEB.port}",
     ]
-
-    MODEL: str = ""
 
     PG = PostgresSettings()
     URI: PostgresDsn = f"postgres://{PG.user}:{PG.password}@{PG.host}/{PG.db}"
