e94a31b07d4ddd957c924fa2e11bc187d6dd6869
Logan Connolly <logan.connolly@protonmail.com>
2020-09-23 18:30:43 +0200

    Make scraper an optional service (#35)
    * Update service and docker image to use scrapy as entrypoint
    
    * Add full helping source if does not exist

--- scraper/scraper/util.py
+++ scraper/scraper/util.py
@@ -1,5 +1,7 @@
-import requests
+import json
+from typing import Dict
 
+import requests
 from bs4 import BeautifulSoup
 
 from .settings import API_URL
@@ -28,3 +30,10 @@ def get_source_id(domain: str) -> int:
     resp = requests.get(f"{API_URL}/sources/?domain={domain}")
     if resp.ok:
         return resp.json()[0]["id"]
+    payload = dict(name="TheFullHelping", url="http://thefullhelping.com")
+    return create_source_id(payload)
+
+
+def create_source_id(payload: Dict[str, str]) -> int:
+    resp = requests.post(f"{API_URL}/sources/", data=json.dumps(payload))
+    return resp.json()["id"]
