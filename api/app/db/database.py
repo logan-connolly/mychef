9423c6f23d08f03a9fcb275e0d255471cde0db2d
Logan Connolly <logan.connolly@protonmail.com>
2020-10-09 12:19:49 +0200

    Do not build tables in app, but use alembic instead

--- api/app/db/database.py
+++ api/app/db/database.py
@@ -1,10 +1,8 @@
 from databases import Database
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import MetaData
 
 from app.core.config import settings
 
 
 metadata = MetaData()
-engine = create_engine(settings.URI)
-metadata.create_all(engine)
 database = Database(settings.URI)
