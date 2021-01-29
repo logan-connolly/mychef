1d9f0073ff6540a82aed5ff821eb52c51714de29
Logan Connolly <logan-victor.connolly@capgemini.com>
2021-01-29 19:59:49 +0100

    Add anchore link to image for accessibility

--- ui/components/RecipeCard.vue
+++ ui/components/RecipeCard.vue
@@ -2,19 +2,12 @@
   <v-container>
     <v-hover>
       <template v-slot:default="{ hover }">
-        <v-card
-          id="recipe__card"
-          class="mx-auto">
-          <v-img
-            :src="recipe.image"
-            :alt="recipe.name"
-          ></v-img>
+        <v-card id="recipe__card" class="mx-auto">
+          <a :href="recipe.url">
+            <v-img :src="recipe.image" :alt="recipe.name"></v-img>
+          </a>
           <v-fade-transition>
-            <v-overlay
-              v-if="hover"
-              absolute
-              color="offwhite"
-              opacity="0.8">
+            <v-overlay v-if="hover" absolute color="offwhite" opacity="0.8">
               <v-row align="center">
                 <v-col class="text-center">
                   <div class="my-2">
@@ -23,10 +16,7 @@
                     </h3>
                   </div>
                   <div class="my-2">
-                    <v-btn
-                      :href="recipe.url"
-                      target="_blank"
-                      color="primary">
+                    <v-btn :href="recipe.url" target="_blank" color="primary">
                       See More
                     </v-btn>
                   </div>
@@ -42,15 +32,15 @@
 
 <script>
 export default {
-  name: 'RecipeCard',
-  props: ['recipe'],
-}
+  name: "RecipeCard",
+  props: ["recipe"]
+};
 </script>
 
 <style scoped>
 #recipe__card {
   border: 3px solid;
-  border-color: rgba(193,200,215,0.3);
-  box-shadow: 3px 3px 8px 0px rgba(0,0,0,0.3);
+  border-color: rgba(193, 200, 215, 0.3);
+  box-shadow: 3px 3px 8px 0px rgba(0, 0, 0, 0.3);
 }
 </style>
