b7efbc0a37090204350553fc7d37a9a378cf2e5e
Logan Connolly <logan.connolly@protonmail.com>
2021-02-26 13:39:35 +0100

    hotfix: update path to model directory

--- api/app/services/models/__init__.py
+++ api/app/services/models/__init__.py
@@ -7,9 +7,10 @@ from loguru import logger
 class Model:
     """Base class for model object to be attached to application"""
 
-    def __init__(self, model_name: str) -> None:
-        self.model_dir = Path(__file__).parent / "version"
-        self.model_path = self.model_dir / model_name
+    directory = Path(__file__).parent
+
+    def __init__(self, model_path: str) -> None:
+        self.model_path = model_path
         if not self.model_path.is_dir():
             self.untar()
 
@@ -18,6 +19,6 @@ class Model:
 
     def untar(self) -> None:
         """Attempt to untar compressed model object directory"""
-        logger.info(f"Attempting to untar model at {self.model_path!r}.tar.gz")
+        logger.info(f"Attempting to untar model at {str(self.model_path)}.tar.gz")
         with tarfile.open(f"{self.model_path}.tar.gz") as tar:
             tar.extractall(path=self.model_path.parent)
--- api/app/services/models/ingredient/__init__.py
+++ api/app/services/models/ingredient/__init__.py
@@ -9,10 +9,11 @@ from app.services.models import Model
 class IngredientExtractor(Model):
     """Named entity recognizer trained to identify ingredients"""
 
-    def __init__(self, path: str):
-        super().__init__(path)
+    def __init__(self, model_name: str):
+        model_path = Model.directory / "ingredient" / "version" / model_name
+        super().__init__(model_path)
         self.nlp = spacy.load(self.model_path)
-        logger.info(f"{path!r} model successfully loaded")
+        logger.info(f"{model_name!r} model successfully loaded")
 
     def extract(self, text: str) -> List[str]:
         """Extract ingredient entities from text"""
