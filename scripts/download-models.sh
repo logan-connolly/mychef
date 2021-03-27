338fcf9bb1f96da0793bfebb60625639ee6b3383
Logan Connolly <logan.connolly@protonmail.com>
2021-03-27 11:23:26 +0100

    chore: add helper script for downloading models

---
+++ scripts/download-models.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PARENTDIR="$(dirname "$DIR")"
+
+INGRED_IMAGE=lvconnolly/mychef_model:ingredient
+INGRED_CONTAINER=tmp_ingred
+INGRED_PATH=$PARENT_DIR/api/app/services/models/ingredient
+
+create_tmp_containers() {
+  echo "Pulling images from registry and starting temp containers ..."
+  docker create -it --name $INGRED_CONTAINER $INGRED_IMAGE sh
+}
+copy_models() {
+  echo "Pulling images from registry and starting temp containers ..."
+  docker cp $INGRED_CONTAINER:/version $INGRED_PATH
+}
+remove_tmp_containers() {
+  echo "Dropping temp containers ..."
+  docker rm -f $INGRED_CONTAINER
+}
+
+create_tmp_containers && copy_models && remove_tmp_containers
