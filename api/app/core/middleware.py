c2694d104b62498fbc73d39ed85ff84668813a8d
Logan Connolly <logan.connolly@protonmail.com>
2021-08-13 21:19:30 +0200

    fix: wrong attribute name in settings

--- api/app/core/middleware.py
+++ api/app/core/middleware.py
@@ -8,8 +8,8 @@ def add_cors_middleware(app: FastAPI):
     if settings.BACKEND_CORS_ORIGINS:
         app.add_middleware(
             CORSMiddleware,
-            allow_origins=settings.BACKEND_CORS_ORIGIN,
-            allow_credentials=True,
+            allow_origins=settings.BACKEND_CORS_ORIGINS,
             allow_methods=["*"],
+            allow_credentials=True,
             allow_headers=["*"],
         )
