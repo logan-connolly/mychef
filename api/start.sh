f508cdc094e438259ec09a26acefbebf3e59cf30
Logan Connolly <logan.connolly@protonmail.com>
2021-09-14 22:16:09 +0200

    chore: merge prestart and start scripts

---
+++ api/start.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env sh
+set -e
+
+# Export variables
+MODULE_NAME=${MODULE_NAME:-app.main}
+VARIABLE_NAME=${VARIABLE_NAME:-app}
+export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
+export GUNICORN_CONF=${GUNICORN_CONF:-/gunicorn_conf.py}
+export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
+
+# Prestart script
+sleep 5
+echo "Running alembic migrations ..."
+alembic upgrade head
+echo "Instantiating meilisearch index ..."
+curl -X POST "http://${SEARCH_HOST}:${SEARCH_PORT}/indexes" --data '{"uid": "recipes"}'
+
+# Start app server
+if [ $DEBUG_SERVER = true ]; then
+	exec uvicorn --reload --host 0.0.0.0 --port 8000 "$APP_MODULE"
+else
+	exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
+fi
