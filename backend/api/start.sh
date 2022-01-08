65960d0a6cb6015c51eb633f291ed0e7b6c840c5
Logan Connolly <logan.connolly@protonmail.com>
2022-01-08 18:40:46 +0100

    chore(api): simplify gunicorn configuration

--- backend/api/start.sh
+++ backend/api/start.sh
@@ -5,8 +5,7 @@ set -e
 MODULE_NAME=${MODULE_NAME:-app.main}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
-export GUNICORN_CONF=${GUNICORN_CONF:-/gunicorn_conf.py}
-export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
+export GUNICORN_CONF=${GUNICORN_CONF:-./gunicorn_conf.py}
 
 # Prestart script
 sleep 5
@@ -19,5 +18,5 @@ curl -X POST "http://${SEARCH_HOST}:${SEARCH_PORT}/indexes" --data '{"uid": "rec
 if [ $DEBUG_SERVER = true ]; then
   exec uvicorn --reload --host 0.0.0.0 --port 8000 "$APP_MODULE"
 else
-  exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
+  exec gunicorn -c "$GUNICORN_CONF" "$APP_MODULE"
 fi
