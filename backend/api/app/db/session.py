1328de998d788b7682b84710ac65682463906ea3
Logan Connolly <logan.connolly@protonmail.com>
2022-02-19 22:00:55 +0100

    feature: add module for yielding async session

---
+++ backend/api/app/db/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+
+engine = create_async_engine(settings.uri)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_db() -> AsyncSession:
+    """Async session to inject into API endpoints"""
+    async with async_session() as session:
+        yield session
+        await session.commit()
