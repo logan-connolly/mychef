3c0b3602c477b2c2af9256b66b6588ab4ab07696
Logan Connolly <logan.connolly@protonmail.com>
2022-02-21 08:36:46 +0100

    refactor: raise HTTP exc on create and not found

--- backend/api/app/api/v1/endpoints/ingredients.py
+++ backend/api/app/api/v1/endpoints/ingredients.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter, Depends
+from app.core.exceptions import DoesNotExist
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio.session import AsyncSession
-from starlette.status import HTTP_200_OK, HTTP_201_CREATED
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+)
 
 from app.db.repositories.ingredients import IngredientsRepository
 from app.db.session import get_db
@@ -14,13 +21,19 @@ async def add_ingredient(
     payload: InIngredientSchema, db: AsyncSession = Depends(get_db)
 ):
     repo = IngredientsRepository(db)
-    return await repo.create(payload)
+    try:
+        return await repo.create(payload)
+    except IntegrityError:
+        raise HTTPException(HTTP_400_BAD_REQUEST, "Ingredient exists")
 
 
 @router.get("/{ingred_id}/", response_model=IngredientSchema, status_code=HTTP_200_OK)
 async def get_ingredient(ingred_id: int, db: AsyncSession = Depends(get_db)):
     repo = IngredientsRepository(db)
-    return await repo.get_by_id(ingred_id)
+    try:
+        return await repo.get_by_id(ingred_id)
+    except DoesNotExist:
+        raise HTTPException(HTTP_404_NOT_FOUND, "Ingredient not found")
 
 
 @router.get("/", response_model=list[IngredientSchema], status_code=HTTP_200_OK)
