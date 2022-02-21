2272954592436ec077fe787ca8c306283ccb3fc3
Logan Connolly <logan.connolly@protonmail.com>
2022-02-21 08:36:46 +0100

    refactor: raise HTTP exc on create and not found

--- backend/api/app/api/v1/endpoints/recipes.py
+++ backend/api/app/api/v1/endpoints/recipes.py
@@ -1,14 +1,17 @@
+from app.core.exceptions import DoesNotExist
 from app.db.repositories.recipes import RecipesRepository
 from app.schemas.recipes import InRecipeSchema, RecipeSchema
 
 import httpx
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi_pagination import Page, paginate
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from starlette.requests import Request
 from starlette.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND,
 )
 
@@ -32,7 +35,9 @@ def extract_ingredients(req: Request, p: InRecipeSchemaRaw) -> InRecipeSchema:
     except AttributeError as err:
         raise HTTPException(HTTP_404_NOT_FOUND, "Ingredient model missing") from err
 
-    return RecipeSchema(name=p.name, url=p.url, image=p.image, ingredients=d)
+    return InRecipeSchema(
+        name=p.name, source_id=p.source_id, url=p.url, image=p.image, ingredients=d
+    )
 
 
 @router.post("/", response_model=RecipeSchema, status_code=HTTP_201_CREATED)
@@ -41,15 +46,23 @@ async def add_recipe(
 ):
     repo = RecipesRepository(db)
     _payload = extract_ingredients(req, payload)
-    recipe = await repo.create(_payload)
-    await send_off_recipe(recipe)
+
+    try:
+        recipe = await repo.create(_payload)
+    except IntegrityError:
+        raise HTTPException(HTTP_400_BAD_REQUEST, "Recipe exists")
+
+    await send_off_recipe(db, recipe)
     return recipe
 
 
 @router.get("/{recipe_id}/", response_model=RecipeSchema, status_code=HTTP_200_OK)
 async def get_recipe(recipe_id: int, db: AsyncSession = Depends(get_db)):
     repo = RecipesRepository(db)
-    return await repo.get_by_id(recipe_id)
+    try:
+        return await repo.get_by_id(recipe_id)
+    except DoesNotExist:
+        raise HTTPException(HTTP_404_NOT_FOUND, "Recipe not found")
 
 
 @router.get("/", response_model=Page[RecipeSchema], status_code=HTTP_200_OK)
@@ -60,16 +73,16 @@ async def get_recipes(db: AsyncSession = Depends(get_db)):
 
 
 # TODO: move this out of module and trigger on event
-async def send_off_recipe(recipe: RecipeSchema) -> None:
+async def send_off_recipe(db: AsyncSession, recipe: RecipeSchema) -> None:
     """Asynchronously send off recipe to ingredients and meilisearch endpoints"""
 
     async def update_ingredients():
         for ingredient in recipe.ingredients["items"]:
-            await add_ingredient(InIngredientSchema(ingredient=ingredient))
+            await add_ingredient(InIngredientSchema(ingredient=ingredient), db=db)
 
     async def update_meili_recipe_index() -> None:
         async with httpx.AsyncClient() as client:
-            await client.post(f"{settings.search_url}", json=recipe.dict())
+            await client.post(settings.search_url, json=[recipe.dict()])
 
     await update_ingredients()
     await update_meili_recipe_index()
--- backend/api/app/api/v1/endpoints/sources.py
+++ backend/api/app/api/v1/endpoints/sources.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter, Depends
+from app.core.exceptions import DoesNotExist
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio.session import AsyncSession
-from starlette.status import HTTP_200_OK, HTTP_201_CREATED
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+)
 
 from app.db.repositories.sources import SourcesRepository
 from app.db.session import get_db
@@ -12,13 +19,19 @@ router = APIRouter()
 @router.post("/", response_model=SourceSchema, status_code=HTTP_201_CREATED)
 async def add_source(payload: InSourceSchema, db: AsyncSession = Depends(get_db)):
     repo = SourcesRepository(db)
-    return await repo.create(payload)
+    try:
+        return await repo.create(payload)
+    except IntegrityError:
+        raise HTTPException(HTTP_400_BAD_REQUEST, "Source exists")
 
 
 @router.get("/{source_id}/", response_model=SourceSchema, status_code=HTTP_200_OK)
 async def get_source(source_id: int, db: AsyncSession = Depends(get_db)):
     repo = SourcesRepository(db)
-    return await repo.get_by_id(source_id)
+    try:
+        return await repo.get_by_id(source_id)
+    except DoesNotExist:
+        raise HTTPException(HTTP_404_NOT_FOUND, "Source not found")
 
 
 @router.get("/", response_model=list[SourceSchema], status_code=HTTP_200_OK)
