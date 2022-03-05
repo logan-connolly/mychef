c86f9d23f3f017f7ddf7ca7f2cf95427824b7f12
Logan Connolly <logan.connolly@protonmail.com>
2022-03-05 16:23:10 +0100

    refactor: improve typing external packages

--- backend/api/app/db/session.py
+++ backend/api/app/db/session.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -7,8 +9,8 @@ engine = create_async_engine(settings.async_uri)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     """Async session to inject into API endpoints"""
     async with async_session() as session:
         yield session
-        session.commit()
+        await session.commit()
