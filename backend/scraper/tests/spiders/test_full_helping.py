ef70ec158cec57b608b373370d4e23c21f4a281b
Logan Connolly <logan.connolly@protonmail.com>
2023-02-24 18:11:13 +0100

    test(scraper): clean up full helping tests

--- backend/scraper/tests/spiders/test_full_helping.py
+++ backend/scraper/tests/spiders/test_full_helping.py
@@ -1,15 +1,24 @@
+from pathlib import Path
+
 import pytest
 
 from scraper.spiders.full_helping_spider import UrlExtractor
 
 
-@pytest.mark.usefixtures("full_helping_recipe_index")
-def test_get_latest_recipe(full_helping_recipe_index, monkeypatch):
-    def mock_get_start_page(url):
-        return full_helping_recipe_index
+@pytest.fixture
+def recipe_index():
+    yield (Path(__file__).parent / "webpages/full_helping.html").read_text()
 
-    monkeypatch.setattr(UrlExtractor, "get_start_page", mock_get_start_page)
 
-    start_url = UrlExtractor("http://thefullhelping.com").get_recipe_url()
-    expected_url = ["https://www.thefullhelping.com/chai-spice-energy-balls/"]
-    assert start_url == expected_url
+@pytest.fixture(autouse=True)
+def patch_full_helping_url(recipe_index, monkeypatch):
+    monkeypatch.setattr(UrlExtractor, "get_start_page", lambda _: recipe_index)
+
+
+def test_get_latest_recipe():
+    extractor = UrlExtractor("http://thefullhelping.com")
+
+    got = extractor.get_start_url()
+    want = "https://www.thefullhelping.com/chai-spice-energy-balls/"
+
+    assert got == want
